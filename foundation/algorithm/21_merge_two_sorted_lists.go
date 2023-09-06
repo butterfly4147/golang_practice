d6491ef68c2826494c6206770ac1618bbe7fbb9c
butterfly-gitee <2271478667@qq.com>
2023-09-06 09:02:04 +0800

    feat: 实现了算法21题解code

--- foundation/algorithm/21_merge_two_sorted_lists.go
+++ foundation/algorithm/21_merge_two_sorted_lists.go
@@ -6,9 +6,29 @@ type ListNode struct {
 	Next *ListNode
 }
 
+// TODO: 空闲了可以试试构造链表节点的CRUD操作
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	dump := &ListNode{}
+	cur := dump
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			cur.Next = list1
+			list1 = list1.Next
+		} else { //include ">="
+			cur.Next = list2
+			list2 = list2.Next
+		}
+		//这一步是为了将cur指向尾节点。因为上述if向cur指向的尾节点后新增了一个节点，形成了cur不是尾节点的情况，需要这一步来进行调整
+		cur = cur.Next
+	}
 
-	return nil
+	if list1 == nil {
+		cur.Next = list2
+	} else {
+		cur.Next = list1
+	}
+
+	return dump.Next
 }
 
 func main() {
