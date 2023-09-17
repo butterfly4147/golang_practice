3811023eb134c053068a832a50b8a857635799a5
butterfly-gitee <2271478667@qq.com>
2023-09-17 19:13:09 +0800

    feat: struct easy testing.

--- foundation_golang/struct_case/struct_test.go
+++ foundation_golang/struct_case/struct_test.go
@@ -2,6 +2,29 @@ package struct_case
 
 import "testing"
 
-func TestStruct(t *testing.T) {
+type abc struct {
+	add func(int, int) int
+	mul func(int, int) int
+}
 
+func TestStruct(t *testing.T) {
+	calc := struct {
+		add func(int, int) int
+		mul func(int, int) int
+	}{
+		func(i int, i2 int) int {
+			return -1
+		},
+		func(i int, i2 int) int {
+			return -1
+		},
+	}
+
+	calc.add(1, 2)
+
+	//
+	calcTest := abc{add: func(i int, i2 int) int {
+		return -1
+	}}
+	calcTest.add(1, 2)
 }
