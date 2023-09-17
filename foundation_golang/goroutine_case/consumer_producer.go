aa805e0ab62b2ffb120cb29c431ec303e15c5602
butterfly-gitee <2271478667@qq.com>
2023-09-17 22:09:22 +0800

    feat: consumer_producer.go ok

--- foundation_golang/goroutine_case/consumer_producer.go
+++ foundation_golang/goroutine_case/consumer_producer.go
@@ -1,4 +1,35 @@
 package main
 
+import "log"
+
 func main() {
+	//store
+	store := make(chan int)
+	done := make(chan bool)
+	//producer
+	go producer(store)
+	//consumer
+	go consumer(store, done)
+
+	//done flag
+	<-done
+	println("store closed")
+}
+
+func consumer(store chan int, done chan bool) {
+	println("start to consume...")
+	for s := range store {
+		log.Printf("consume %d\n", s)
+	}
+	done <- true
+}
+
+func producer(store chan int) {
+	println("start to produce...")
+	for i := 0; i < 4; i++ {
+		log.Printf("produce %d\n", i)
+		store <- i
+	}
+
+	close(store)
 }
