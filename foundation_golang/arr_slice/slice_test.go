d4cd1a18413d1c20d9f199db3ed2bdfd370b6714
butterfly-gitee <2271478667@qq.com>
2023-09-25 16:38:56 +0800

    feat: some slice tricks, ok.

---
+++ foundation_golang/arr_slice/slice_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+//FILO: first in last out
+
+type Stack []int
+
+func NewStack() *Stack {
+	s := make(Stack, 0, 10)
+	return &s
+}
+
+func (s *Stack) Push(v int) {
+	*s = append(*s, v)
+}
+
+func TestSliceStack(t *testing.T) {
+	s := NewStack()
+	fmt.Printf("%v\n", s)
+}
+
+func TestFilter(t *testing.T) {
+	//n := 0
+	//for _, x := range a {
+	//	if keep(x) {
+	//		a[n] = x
+	//		n++
+	//	}
+	//}
+	//a = a[:n]
+}
+
+func TestReverse(t *testing.T) {
+	a := []int{1, 2, 34, 2, 34, 2, 34, 2, 34}
+
+	for i := len(a)/2 - 1; i >= 0; i-- {
+		opp := len(a) - 1 - i
+		a[i], a[opp] = a[opp], a[i]
+	}
+}
+
+func TestSlidingWindow(t *testing.T) {
+	window := slidingWindow(2, []int{1, 2, 3, 4, 5, 56, 6, 2})
+	fmt.Println(window)
+}
+
+func slidingWindow(size int, input []int) [][]int {
+	// returns the input slice as the first element
+	if len(input) <= size {
+		return [][]int{input}
+	}
+
+	// allocate slice at the precise size we need
+	r := make([][]int, 0, len(input)-size+1)
+
+	for i, j := 0, size; j <= len(input); i, j = i+1, j+1 {
+		r = append(r, input[i:j])
+	}
+
+	return r
+}
