60f68003f1ae8c62147107008229f2bfffe07ecc
butterfly-gitee <2271478667@qq.com>
2023-10-22 21:33:53 +0800

    feat: Study 'builder designer-pattern' > OK.

--- foundation_golang/goto/goto_test.go
+++ foundation_golang/goto/goto_test.go
@@ -38,3 +38,45 @@ Skip:
 		i++
 	}
 }
+
+type Task struct {
+	Cid int64
+}
+
+func TestGotoSlice(t *testing.T) {
+	//ConflictTaskSlice := make(map[int64]bool)
+	ConflictTaskSlice := make([]int64, 0)
+	TaskInfo := make([]*Task, 0)
+	isConflict := false
+
+	//ConflictTaskSlice[1] = true
+	//ConflictTaskSlice[2] = true
+	ConflictTaskSlice = append(ConflictTaskSlice, 1)
+	ConflictTaskSlice = append(ConflictTaskSlice, 2)
+
+	TaskInfo = append(TaskInfo, &Task{Cid: 2})
+	TaskInfo = append(TaskInfo, &Task{Cid: 1})
+
+loop:
+	//break会跳出两个for
+	for idx1, conflictCid := range ConflictTaskSlice {
+		println(idx1, conflictCid)
+		//if isConflict {
+		//	break
+		//}
+		//loop:
+		//loop:
+		//break会跳出内层for，并将外层for加一
+		for idx2, curTask := range TaskInfo {
+			println(idx2, conflictCid)
+			if conflictCid == curTask.Cid { // 和已有任务冲突
+				isConflict = true
+				//ss.Debugf("任务-新任务cid: %+v 和已有任务: %+v 冲突", newTaskCid, curTask)
+				break loop // 【跳出外层的 for 循环】break loop是指定break到哪一层for循环。loop标签在内层for前一行，则break跳出内层循环；在外层for前一行，则break跳出外层for循环
+			}
+		}
+	}
+
+	//loop:
+	println("isConflict: ", isConflict)
+}
